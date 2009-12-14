943e11b6493bef7483702548452d123fa2a0fd14
Benoit Chesneau <benoitc@enlil.local>
2009-12-14 17:36:10 +0100

    display server info in couch_res_db rather that in its own resource. Will allow to handle POST /

--- src/couchweb/couchweb_res_db.erl
+++ src/couchweb/couchweb_res_db.erl
@@ -17,16 +17,19 @@
     content_types_provided/2,
     content_types_accepted/2,
     resource_exists/2,
+    post_is_create/2,
+    create_path/2,
     is_authorized/2,
     to_json/2,
-    to_text/2
+    to_text/2,
+    from_json/2
 ]).
 
 -include_lib("couchdb/couch_db.hrl").
 -include_lib("webmachine/webmachine.hrl").
 
 -record(ctx, {
-    db,
+    db=nil,
     user_ctx
 }).
 
@@ -38,26 +41,48 @@ content_types_provided(RD, Ctx) ->
       {"text/plain", to_text},
       {"text/html", to_text}],
      RD, Ctx}.
+    
+    
      
 content_types_accepted(RD, Ctx) ->
-    {[{"applicatiob/json", handle_incoming}], RD, Ctx}.
+    {[{"applicatiob/json", from_json}], RD, Ctx}.
+
+post_is_create(RD, Ctx) ->
+    {true, RD, Ctx}.    
+  
+create_path(RD, Ctx) ->
+     DbName = wrq:path_info(dbname, RD),
+     {DbName, RD, Ctx}.
+     
     
 allowed_methods(RD, Ctx) ->
-    {['GET', 'HEAD', 'DELETE', 'POST', 'PUT'], 
+    {['GET', 'HEAD', 'DELETE', 'PUT'], 
     RD, Ctx}.
     
 is_authorized(RD, Ctx) ->
     {true, RD, Ctx}.
      
 resource_exists(RD, Ctx) ->
-    DbName = wrq:path_info(dbname, RD),
-    UserCtx =  #user_ctx{}, 
-    case couch_db:open(?l2b(DbName),  [{user_ctx, UserCtx}]) of
-        {ok, Db} ->
-            {true, RD, Ctx#ctx{db=Db}};
-        _Error ->
-            {false, RD, Ctx}
+    case wrq:path_info(dbname, RD) of
+        undefined ->
+            {true, RD, Ctx};
+        DbName ->
+            UserCtx =  #user_ctx{}, 
+            case couch_db:open(?l2b(DbName),  [{user_ctx, UserCtx}]) of
+                {ok, Db} ->
+                    {true, RD, Ctx#ctx{db=Db}};
+                _Error ->
+                    {false, RD, Ctx}
+            end
     end.
+        
+
+to_json(RD, Ctx=#ctx{db=nil}) ->
+    Body = {[
+        {couchdb, <<"Welcome">>},
+        {version, list_to_binary(couch_server:get_version())}
+    ]},
+    {?JSON_ENCODE(Body) ++ <<"\n">>, RD, Ctx};        
        
 to_json(RD, Ctx=#ctx{db=Db}) ->
     {ok, DbInfo} = couch_db:get_db_info(Db),
@@ -68,6 +93,9 @@ to_text(RD, C0) ->
     {Json, RD1, C1} = to_json(RD, C0),
     {json_pp:print(binary_to_list(list_to_binary(Json))), RD1, C1}.
     
-handle_incoming(RD, Ctx) ->
+from_json(RD, Ctx) ->
+    
+    
+    
     {RD, Ctx}.
     
