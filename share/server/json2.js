79d5050aedb230c92347c68c8dd4332d9e02ac9b
Benoit Chesneau <benoitc@enlil.local>
2009-12-21 00:36:43 +0100

    fix e4x error

--- share/server/json2.js
+++ share/server/json2.js
@@ -183,12 +183,19 @@ if (!this.JSON) {
                  f(this.getUTCSeconds())   + 'Z' : null;
         };
 
-        String.prototype.toJSON =
         Number.prototype.toJSON =
         Boolean.prototype.toJSON = function (key) {
             return this.valueOf();
         };
-    }
+        
+    }     
+    String.prototype.toJSON = function(key) {
+        val = this.valueOf();
+        if (typeof val === 'xml') {
+          return val.toXMLString();
+        }
+        return val;
+    };
 
     var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
         escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
